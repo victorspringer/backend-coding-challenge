d9fcc7efbc2a372e3f0c60ceea71335ba3b53308
Victor Springer <victorspringer@gmail.com>
2024-06-03 02:36:12 +0200

    update placeholder pages

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -3,10 +3,8 @@ import Typography from '@mui/material/Typography';
 
 export default function Index() {
   return (
-    <>
-      <Typography className="mrs" variant="h4" component="h1" sx={{ mb: 2 }}>
-        <span>Movie</span> <span>Rating</span> <span>System</span>
-      </Typography>
-    </>
+    <Typography className="mrs" variant="h4" component="h1" sx={{ mb: 2 }}>
+      <span>Movie</span> <span>Rating</span> <span>System</span>
+    </Typography>
   );
 }
--- client/pages/users.tsx
+++ client/pages/users.tsx
@@ -1,20 +1,10 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import Link from '../src/Link';
-import Box from '@mui/material/Box';
 
 export default function Users() {
   return (
-    <>
-      <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-        Users page
-      </Typography>
-      <Box sx={{ maxWidth: 'sm' }}>
-        <Button variant="contained" component={Link} noLinkStyle href="/">
-          Go to the home page
-        </Button>
-      </Box>
-    </>
+    <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+      Users page
+    </Typography>
   );
 }
